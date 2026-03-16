28b81e33acb1f8c4a283c3dd6f50f57980e58c99
Lucas_Arora <kumarhimanshyprajapati@gmail.com>
2026-03-16 09:37:53 +0530

    Update rank of Arushkumar_Kumar from 8 to 9

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let members = JSON.parse(localStorage.getItem("members")) || [
 {name:"Lucas_Arora",rank:10,level:40,password:"lucas9389",money:0,war:0},
-{name:"Arushkumar_Kumar",rank:8,level:36,password:"arush6327",money:0,war:0},
+{name:"Arushkumar_Kumar",rank:9,level:36,password:"arush6327",money:0,war:0},
 {name:"Don_Abhishek",rank:8,level:19,password:"abhishek7419",money:120,war:0},
 {name:"Sheikh_Himanshu",rank:8,level:7,password:"himanshu8642",money:0,war:0},
 {name:"Pradyum_Ivanov",rank:8,level:24,password:"pradyum5931",money:0,war:0}
